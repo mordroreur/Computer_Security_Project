cae8b1038207c6dc3403dd552da021069f50749b
mordrtoreur <mlaurencot@gmail.com>
2025-04-30 11:01:09 -0500

    compile from everywhere

--- client/includes/version.h
+++ client/includes/version.h
@@ -1,6 +1,6 @@
 #ifndef __MORDROREUR_VERSION_H__
 #define __MORDROREUR_VERSION_H__
 
-#define VERSION_STRING "0.0.0-8e821e3-dirty"
+#define VERSION_STRING "0.0.0-96c31e9"
 
 #endif // __MORDROREUR_VERSION_H__
